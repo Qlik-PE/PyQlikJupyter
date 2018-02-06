98f51ed5df1115961f7f1afbb346764aae58c9de
Chris Larsen <chris.larsen@qlik.com>
2018-02-05 20:42:51 -0600

    Update TestPyQlik.py

--- TestPyQlik.py
+++ TestPyQlik.py
@@ -1,8 +1,3 @@
-from pyqlikengine.engine_communicator import EngineCommunicator
-from pyqlikengine.engine_global_api import EngineGlobalApi
-from pyqlikengine.engine_app_api import EngineAppApi
-from pyqlikengine.engine_communicator import SecureEngineCommunicator
-
 host = "cloudera.qlik.com"
 proxyPrefix = "jupyter"
 userDirectory = "CLOUDERA"
@@ -37,7 +32,7 @@ opened_app = ega.open_doc('8921dfe7-f46c-437c-bdb3-eb16c768793f') ##Executive Da
 app_handle = ega.get_handle(opened_app)
 
 ### Define Dimension of hypercube
-hc_inline_dim = Structs.nx_inline_dimension_def(["Order Number"])
+hc_inline_dim = Structs.nx_inline_dimension_def(["Customer"])
 
 ### Set sorting of Dimension by Measure
 hc_mes_sort = Structs.nx_sort_by()
@@ -48,7 +43,7 @@ hc_inline_mes = Structs.nx_inline_measure_def("=Sum([Sales Amount])")
 ### Build hypercube from above definition
 hc_dim = Structs.nx_hypercube_dimensions(hc_inline_dim)
 hc_mes = Structs.nx_hypercube_measure(hc_mes_sort, hc_inline_mes)
-nx_page = Structs.nx_page(0, 0, 100, 2)
+nx_page = Structs.nx_page(0, 0, 100, 3)
 hc_def = Structs.hypercube_def("$", [hc_dim],[hc_mes], [nx_page])
 hc_response = eaa.create_object(app_handle, "CH01", "Chart", "qHyperCubeDef", hc_def)
 hc_handle = ega.get_handle(hc_response)
@@ -68,6 +63,7 @@ sel_res = efa.select_values(fld_handle,values_to_select)
 hc_data = egoa.get_hypercube_data(hc_handle, "/qHyperCubeDef", [nx_page])
 
 elems = hc_data["qDataPages"][0]['qMatrix']
+
 dim_list = []
 mes_list = []
 
@@ -95,11 +91,20 @@ import pandas as pd
 import seaborn as sns
 
 ### Load data
-d = {'orders':dim_list, 'sales':mes_list} 
+d = {'customer':dim_list, 'sales':mes_list} 
 df = pd.DataFrame(d)
 
 ### Set up a factorplot
 #g = sns.factorplot(dim_list, mes_list,  data=df, kind="bar", palette="muted", legend=False)
 
 ### Try a StripPlot
-sns.stripplot(x='orders', y='sales', data=df);
+sns.stripplot(x='customer', y='sales', data=df);
+
+#import matplotlib.pyplot as plt
+#
+#dims=['a','b','c','d','e']
+#meas=[1,2,3,4,5]
+#
+#plt.bar(range(len(dims)), meas)
+#plt.xticks(range(len(dims)), dims)
+#plt.show()
