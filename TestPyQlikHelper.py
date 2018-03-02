a5fb789c4771ecdced8f9f4fc6849dfb298c69f8
Chris Larsen <chris.larsen@qlik.com>
2018-03-01 19:33:02 -0800

    Update TestPyQlikHelper.py

--- TestPyQlikHelper.py
+++ TestPyQlikHelper.py
@@ -2,26 +2,24 @@
 # !pip install -r requirements.txt
 
 ### Instantiate PyQIX
-from pyqlikengine import instantiate_helper
+from pyqlikengine import instantiate_helper as helper
 
 ### Get a list of apps
-apps = ega.get_doc_list()
+apps = helper.ega.get_doc_list()
 
 ### List Apps available (identify the App GUID to open)
 for app in apps:
     print app['qTitle']+'-'+app['qDocId']
     
-opened_app = ega.open_doc('8921dfe7-f46c-437c-bdb3-eb16c768793f') ##Executive Dashboard
-app_handle = ega.get_handle(opened_app)
+opened_app = helper.ega.open_doc('8921dfe7-f46c-437c-bdb3-eb16c768793f') ##Executive Dashboard
+app_handle = helper.ega.get_handle(opened_app)
 
 
 from pyqlikengine import engine_helper as pyqlikhelper
 
 dimensions = ["Customer","Order Number"]
 measures = ["=Sum([Sales Amount])", "=Avg([Sales Amount])"]
-selections = {"Customer": ["Sigma"]}
-df = pyqlikhelper.getDataFrame(conn, app_handle, measures, dimensions, selections)
-conn.close_qvengine_connection(conn)
+df = pyqlikhelper.getDataFrame(helper.conn, app_handle, measures, dimensions)
 
 ############ VISUALIZE #############
 
