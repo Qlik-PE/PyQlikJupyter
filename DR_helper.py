00f8e071c4e6b3687eed362f00838a370ffe6a45
Chris Larsen <chris.larsen@qlik.com>
2018-04-18 00:28:19 -0500

    Update DR_helper.py

--- DR_helper.py
+++ DR_helper.py
@@ -10,3 +10,21 @@ dr.Client(token=token, endpoint='%s/api/v2' % host)
 # Prediction API parameters
 prediction_host = 'https://qlik.orm.datarobot.com'
 headers = {'Content-Type': 'application/json', 'datarobot-key': '94d2823a-cd48-8be3-03de-fd0f58515e66'}
+
+loss_model_id = "5ad4d867c2674e17de32822a"
+loss_project_id = "5ad4d836e3cd9b0e5e90b89f"
+
+conversion_model_id = "5ad4d9a6c2674e476194eb47"
+conversion_project_id = "5ad4d933e3cd9b11099173dc"
+
+# Prediction API function for 1 JSON
+def predict_API_call(host, headers, username, token, model_id, project_id, data, classif=True):
+    response = requests.post('%s/api/v1/%s/%s/predict' % (host, project_id, model_id),
+                            auth=(username, token), data=data, headers=headers)
+    output = response.json()
+    if classif:
+        predictions = [record['class_probabilities']['1.0'] 
+                       for record in output['predictions']]
+    else:
+        predictions = [record['prediction'] for record in output['predictions']]
+    return predictions
